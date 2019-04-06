17ae47766c5433d65c445ee6205d76a941617439
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-06 22:14:42 +0200

    Update Main.java

--- LAB3/Main.java
+++ LAB3/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-  Employee Kacper = new Employee(1,"Kacper","Wieczorek",5000);
-        InvoiceItem Aleksander = new InvoiceItem(1,"opis",5000,2);
+        Employee Kacper = new Employee(1, "Kacper", "Wieczorek", 5000);
+        InvoiceItem Aleksander = new InvoiceItem(1, "opis", 5000, 2);
+        Date Kiedys = new Date(28, 8, 1997);
+        Account Konto = new Account("1", "Konto osobiste", 5000);
+        Time Zegarek = new Time(5, 23, 45);
         System.out.println(Kacper);
         System.out.println(Aleksander);
-          }
+        System.out.println(Kiedys);
+        System.out.println(Konto);
+        System.out.println(Zegarek);
+    }
 }
