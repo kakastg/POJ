d16cf262ca04964087eb710e48ee530375c96a30
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-06 15:56:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
   Employee Kacper = new Employee(1,"Kacper","Wieczorek",5000);
+        InvoiceItem Aleksander = new InvoiceItem(1,"opis",5000,2);
+        System.out.println(Kacper);
+        System.out.println(Aleksander);
           }
 }
