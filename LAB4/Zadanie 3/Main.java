d69829f34392fee5e46c000ed039a968b04832d2
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 16:02:20 +0200

    Create Main.java

---
+++ LAB4/Zadanie 3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Point punkcik=new Point (1f,2f);
+        MovablePoint innypunkt = new MovablePoint(0.5f,1.3f);
+    }
+}
