9f575ce356117ee80c4aa1d7506d29b8ae83200d
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 16:07:15 +0200

    Update Main.java

--- LAB4/Zadanie 3/Main.java
+++ LAB4/Zadanie 3/Main.java
@@ -1,3 +1,9 @@
+/*
+    Klaudia Klebba
+    Kacper Wieczorek
+    Zadanie 1 lab4
+    Tworzenie klasy Person/Student/Staff
+     */
 public class Main {
 
     public static void main(String[] args)
