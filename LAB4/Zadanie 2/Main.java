b578075d034ea8894562cb34ef04e98c0c0c7990
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 21:07:36 +0200

    Create Main.java

---
+++ LAB4/Zadanie 2/Main.java
@@ -0,0 +1,16 @@
+/*
+* Autorzy: Klaudia Klebba
+*          Kacper Wieczorek
+* Zadnie 2 POINT2D
+* 
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D punkto = new Point2D(32.3f,4.5f);
+        Point3D punkter = new Point3D(1.3f,4.6f,8.6f);
+        System.out.println(punkto);
+        System.out.println(punkter);
+    }
+}
