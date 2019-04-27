9ca669d17e0b5d688883e12e80ae876865d6c6cc
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 16:05:32 +0200

    Create MovablePoint.java

---
+++ LAB4/Zadanie 3/MovablePoint.java
@@ -0,0 +1,54 @@
+public class MovablePoint extends Point {
+    private float xSpeed=0.0f;
+    private float ySpeed=0.0f;
+
+    public MovablePoint(float x, float y,float xSpeed,float ySpeed)
+    {
+        super(x,y);
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public MovablePoint(float xSpeed,float ySpeed)
+    {
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public MovablePoint()
+    {
+        xSpeed=0.0f;
+        ySpeed=0.0f;
+    }
+    public float getXSpeed()
+    {
+        return xSpeed;
+    }
+    public void setXSpeed(float xSpeed)
+    {
+        this.xSpeed=xSpeed;
+    }
+    public float getYSpeed()
+    {
+        return ySpeed;
+    }
+    public void setYSpeed(float ySpeed)
+    {
+        this.ySpeed=ySpeed;
+    }
+        public void setSpeed(float xSpeed,float ySpeed)
+    {
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public float[] getSpeed={xSpeed,ySpeed};
+
+    public String toString()
+    {
+        return "("+getX()+","+getY()+"), speed="+xSpeed+","+ySpeed+")";
+    }
+    public MovablePoint move()
+    {
+        setX(getX()+xSpeed);
+        setY(getY()+ySpeed);
+        return this;
+    }
+}
