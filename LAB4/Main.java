4aa4557ebb209f146f957ae9db029f2564385626
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 15:15:19 +0200

    Create Main.java

---
+++ LAB4/Main.java
@@ -0,0 +1,17 @@
+/*
+    Klaudia Klebba
+    Kacper Wieczorek
+    Zadanie 1 lab4
+    Tworzenie klasy Person/Student/Staff
+     */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person Kacper = new Person ("Kacper","Nibylandia");
+        Student Klaudia = new Student ("Klaudia","Disneyland","Ochrony",2019,5);
+        Staff Martyna = new Staff ("Martyna","Gdansk","PJATK",500);
+
+
+    }
+}
