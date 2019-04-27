b205f29c75d047964f1db9cd190f4441183c7569
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-04-27 21:17:48 +0200

    Create Armstrong.java

---
+++ LAB4/Zadanie 5/Armstrong.java
@@ -0,0 +1,26 @@
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        int liczba = 153;
+
+        int pocz_liczba, reszta, wynik = 0, n = 0;
+
+        pocz_liczba = liczba;
+
+        for (;pocz_liczba != 0; pocz_liczba /= 10, ++n);
+
+        pocz_liczba = liczba;
+
+        for (;pocz_liczba != 0; pocz_liczba /= 10)
+        {
+            reszta = pocz_liczba % 10;
+            wynik += Math.pow(reszta, n);
+        }
+
+        if(wynik == liczba)
+            System.out.println(liczba + " is an Armstrong number.");
+        else
+            System.out.println(liczba + " is not an Armstrong number.");
+    }
+}
