17ff9bd37f1dff243d68e69b12f7aec1ed38e35f
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-11 16:26:57 +0200

    Create MyCircle.java

---
+++ LAB5/Zadanie 2/MyCircle.java
@@ -0,0 +1,69 @@
+/*
+    Dorota Matkowska
+    Kacper Wieczorek
+    Zadanie 1 lab5
+    Klasa MyPoint
+     */
+public class MyCircle {
+    private MyPoint center;
+    private int radius = 1;
+
+    public MyCircle(){}
+
+    public MyCircle (int x, int y, int radius)
+    {
+        this.center=new MyPoint(x,y);
+        this.radius=radius;
+    }
+    public MyCircle(MyPoint center, int radius)
+    {
+        this.center=center;
+        this.radius=radius;
+    }
+    public int getRadius()
+    {
+        return radius;
+    }
+    public void setRadius(int radius)
+    {
+        this.radius=radius;
+    }
+    public MyPoint getCenter()
+    {
+        return center;
+    }
+
+    public void setCenter(MyPoint center) {
+        this.center = center;
+    }
+    public int getCenterX()
+    {
+        return center.getX();
+    }
+    public int getCenterY()
+    {
+        return center.getY();
+    }
+    public void setCenterXY(int x, int y)
+    {
+        this.center.setXY(x,y);
+    }
+    public double getArea()
+    {
+        return Math.PI*radius*radius;
+    }
+    public String toString()
+    {
+        return "MyCircle [promień="+radius+", środek="+center.toString()+"]";
+    }
+    public double getCircumference()
+    {
+        return 2*Math.PI*radius;
+    }
+    public double distance(MyCircle another)
+    {
+        int xdis = this.center.getX() - another.getCenterX();
+        int ydis = this.center.getY() - another.getCenterY();
+        return Math.sqrt(xdis * xdis + ydis * ydis);
+    }
+}
