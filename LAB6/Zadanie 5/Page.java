4ada74a2df14d6e345284ec16050044dff8458f7
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:21:35 +0200

    Create Page.java

---
+++ LAB6/Zadanie 5/Page.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Page implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Going to the next page");
+    }
+}
