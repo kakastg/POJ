ed535cb50da1ba9fb65bfa9f3433c5733e25dee2
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:22:26 +0200

    Create DemoTurners2.java

---
+++ LAB6/Zadanie 5/DemoTurners2.java
@@ -0,0 +1,26 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class DemoTurners2 {
+    public static void main(String[] args)
+    {
+        Leaf lisc = new Leaf();
+        lisc.turn();
+
+        Page strona = new Page();
+        strona.turn();
+
+        Pancake nalesnik = new Pancake();
+        nalesnik.turn();
+
+        Car auto = new Car();
+        auto.turn();
+
+        Wheel kolo = new Wheel();
+        kolo.turn();
+
+    }
+}
