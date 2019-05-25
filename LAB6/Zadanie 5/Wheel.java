a27c1ad3e0fd3188def56494c44009191f47518e
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:22:41 +0200

    Create Wheel.java

---
+++ LAB6/Zadanie 5/Wheel.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Wheel implements Turner{
+    @Override
+    public void turn() {
+        System.out.println("The wheels on the bus go round and round (...). All through the town");
+    }
+}
