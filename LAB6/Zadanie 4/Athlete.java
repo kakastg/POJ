b0dc10585f58333ee9205ed6a33a423aea518a1e
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:01:12 +0200

    Create Athlete.java

---
+++ LAB6/Zadanie 4/Athlete.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 4 lab6
+    Interface Runner
+     */
+public class Athlete implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Athlete literally runs on his feet to be the fastest");
+    }
+}
