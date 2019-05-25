6a2a3ca8deff11e5a0f497bd097cec0800b43f91
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:21:53 +0200

    Create Pancake.java

---
+++ LAB6/Zadanie 5/Pancake.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Pancake implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Flipping");
+    }
+}
