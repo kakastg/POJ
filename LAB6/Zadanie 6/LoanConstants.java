dbd665bd4473df7a4c2692dd5aeee92f4fbd73da
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 19:19:57 +0200

    Create LoanConstants.java

---
+++ LAB6/Zadanie 6/LoanConstants.java
@@ -0,0 +1,13 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 6 lab6
+    Interface, abstrakcja Loan
+     */
+public interface LoanConstants {
+     int SHORT_TERM = 1;
+     int MEDIUM_TERM = 3;
+     int LONG_TERM = 5;
+     String COMPANY_NAME = "Sanchez Construction Loan Co.";
+     double MAX_LOAN_AMOUNT = 100000.00;
+}
