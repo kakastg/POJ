c24fb6a154e387113b7610dce1850626ac8a0934
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 15:12:10 +0200

    Create GeometricObject.java

---
+++ LAB6/Zadanie 2/GeometricObject.java
@@ -0,0 +1,10 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 2 lab6
+    Interface GeometricObject
+     */
+public interface GeometricObject {
+    double getPerimeter();
+    double getArea();
+}
