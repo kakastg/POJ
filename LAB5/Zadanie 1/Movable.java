4528ed1c441d39826661bc9087e4824cb845377d
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 14:43:31 +0200

    Create Movable.java

---
+++ LAB5/Zadanie 1/Movable.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 1 lab6
+    Interface Movable
+     */
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
