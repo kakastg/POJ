4345de07b915db2bc4a3046889dc9397eedfe45d
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:01:36 +0200

    Create PoliticalCandidate.java

---
+++ LAB6/Zadanie 4/PoliticalCandidate.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 4 lab6
+    Interface Runner
+     */
+public class PoliticalCandidate implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Political Candidate runs in elections to become president");
+    }
+}
