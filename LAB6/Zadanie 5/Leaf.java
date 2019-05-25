f8e431f398c5f025b9fb45d196e9275a2f49c2e0
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:21:12 +0200

    Create Leaf.java

---
+++ LAB6/Zadanie 5/Leaf.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Leaf implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Changing colors");
+    }
+}
