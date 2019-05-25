2f7c49e4abf68c6796e68860e37e0073676964c7
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:01:00 +0200

    Create Machine.java

---
+++ LAB6/Zadanie 4/Machine.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 4 lab6
+    Interface Runner
+     */
+public class Machine implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Machine operates using electricity");
+    }
+}
