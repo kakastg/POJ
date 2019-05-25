2e9fa046265f5d99082a1c61fd360f2c85972a17
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-05-25 16:22:53 +0200

    Create Car.java

---
+++ LAB6/Zadanie 5/Car.java
@@ -0,0 +1,12 @@
+/*
+    Martyna Klebba
+    Kacper Wieczorek
+    Zadanie 5 lab6
+    Interface Turner
+     */
+public class Car implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Taking a turn");
+    }
+}
