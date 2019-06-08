4b8c4c7d97db3764f7f151d7c1db6216476e7539
Kacper Wieczorek <48858997+kakastg@users.noreply.github.com>
2019-06-08 19:49:38 +0200

    Update AirlineTest.java

--- LAB7/Zadanie 1/AirlineTest.java
+++ LAB7/Zadanie 1/AirlineTest.java
@@ -1,3 +1,9 @@
+/*
+    Aleksander Mojzych
+    Kacper Wieczorek
+    Zadanie 1 lab7
+    Implementacja Adaptera z Liniami Lotniczymi
+     */
 import java.time.LocalDateTime;
 
 public class AirlineTest {
